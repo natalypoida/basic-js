7d0f2ceec7cfecaaef112710d9570f85bd0808ff
Natalya POida <nataly.poida@gmail.com>
2021-03-04 16:57:04 +0300

    refactor: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,18 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false;
+
+  let sampleActivityNum = parseFloat(sampleActivity);
+  if (
+    isNaN(sampleActivityNum) ||
+    sampleActivityNum <= 0 ||
+    sampleActivityNum > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY / sampleActivityNum) / (0.693 / HALF_LIFE_PERIOD)
+  );
 };
